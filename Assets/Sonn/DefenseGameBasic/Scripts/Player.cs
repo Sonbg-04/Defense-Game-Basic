60cf6cc834aff57da097ee71f85f6f44ca5cf047
Son <lvson21102k4@gmail.com>
2025-07-19 09:38:28 +0700

    Add Const and Player Script
    Add Const and Player Script

---
+++ Assets/Sonn/DefenseGameBasic/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using Sonn.DefenseGameBasic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sonn.DefenseGameBasic
+{
+    public class Player : MonoBehaviour
+    {
+        public float attackRate;
+        private Animator m_anim;
+        private float m_curAttackrate;
+        private bool m_isAttacked;
+
+        private void Awake()
+        {
+            m_anim = GetComponent<Animator>();
+            m_curAttackrate = attackRate;
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !m_isAttacked)
+            {
+                if (m_anim)
+                {
+                    m_anim.SetBool(Const.ATTACK_ANIMATION, true);
+                }
+                m_isAttacked = true;
+            }
+            if (m_isAttacked)
+            {
+                m_curAttackrate -= Time.deltaTime;
+                if (m_curAttackrate <= 0)
+                {
+                    m_isAttacked = false;
+                    m_curAttackrate = attackRate;
+                }
+            }
+        }
+
+        public void Reset_Attack_Animator()
+        {
+            if (m_anim)
+            {
+                m_anim.SetBool(Const.ATTACK_ANIMATION, false);
+            }
+        }
+    }
+
+}
+
