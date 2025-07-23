e21b0eb9fbb883696054765e3d88f573fea8963d
Son <lvson21102k4@gmail.com>
2025-07-23 12:40:54 +0700

    Update
    Pause_Dialog Script

--- Assets/Sonn/DefenseGameBasic/Scripts/UI/PauseDialog.cs
+++ Assets/Sonn/DefenseGameBasic/Scripts/UI/PauseDialog.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Sonn.DefenseGameBasic
 {
     public class PauseDialog : Dialog
     {
+        public override void Show(bool isShow)
+        {
+            Time.timeScale = 0f;
+            base.Show(isShow);
+        }
 
+        public override void CloseDialog()
+        {
+            Time.timeScale = 1f;
+            base.CloseDialog();
+        }
+        public void ResumeGame()
+        {
+            CloseDialog();
+        }
+        public void Replay()
+        {
+            CloseDialog();
+            SceneManager.LoadScene(Const.GAME_PLAY_SCREEN);
+        }    
     }
 
 }
